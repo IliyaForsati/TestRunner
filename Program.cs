11cbe2166b7f9e406750323a659b8956366cc865
Iliya Forsati <iliyaforsati2005@gmail.com>
2025-05-12 14:54:54 +0330

    add some feature

--- Program.cs
+++ Program.cs
@@ -1,14 +1,15 @@
-﻿using System.Collections.Concurrent;
+﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Text.RegularExpressions;
-using System.Text;
-using System.Threading;
-using System;
 using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
 
-namespace TestRunner
+namespace RTProSL_TestRunner
 {
     /// <summary>
     /// This project is designed to run tests concurrently across multiple threads.
@@ -24,19 +25,28 @@ namespace TestRunner
         private static ThreadState[] threads;
         private static ConcurrentQueue<string> TestsQueue;
 
-        private static Dictionary<string, string> Projects = new Dictionary<string, string>(){
-            { "MS_TEST", "C:\\Users\\Afshin\\Desktop\\project\\RTProSL-Test\\RTProSL-MSTest\\bin\\Debug\\net7.0"},
-            { "", ""}
+        private static Dictionary<int, string> Projects = new Dictionary<int, string>(){
+            { 1 , @".\..\..\..\RTProSL-MSTest\bin\Debug\net7.0"},
+            { 2 , @".\..\..\..\RTProSL-MSTest-Reports\bin\Debug\net7.0"}
         };
 
         public static void Main(string[] args)
         {
+            // calculate total time
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
             // Main method responsible for setting up threads and managing the overall test execution process
+            Console.Write("How many threads do you want to use?");
             numberOfThreads = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Which project do you want to run?\n  1-RTProSL_MSTest\n  2-RTProSL_MSTest_Reports");
+            int project = int.Parse(Console.ReadLine());
+
             threads = new ThreadState[numberOfThreads];
 
             var TestsLst = findTestClasses();
-            TestsQueue = new ConcurrentQueue<string>(TestsLst.Take(5).ToList());
+            TestsQueue = new ConcurrentQueue<string>(TestsLst.Take(3).ToList());
 
             for (int i = 0; i < numberOfThreads; i++)
             {
@@ -45,7 +55,7 @@ namespace TestRunner
                 threads[i] = new ThreadState();
 
                 string dirName = $"thread#{id + 1}_results";
-                Process.Start("cmd.exe", $@"/c robocopy {Projects["MS_TEST"]} {dirName} /E").WaitForExit();
+                Process.Start("cmd.exe", $@"/c robocopy {Projects[project]} {dirName} /E").WaitForExit();
                 threads[i].Dir = Path.Combine(dirName, "RTProSL-MSTest.dll");
 
                 threads[i].Thread = new Thread(() => WorkerLoop(id));
@@ -59,10 +69,20 @@ namespace TestRunner
                 Console.WriteLine($"Thread #{i + 1}: Done :)");
                 Console.ResetColor();
 
+                // delete the created directory
                 string directory = Path.GetDirectoryName(threads[i].Dir);
                 if (Directory.Exists(directory))
                     Directory.Delete(directory, true);
             }
+
+
+            // stop timer
+            stopwatch.Stop();
+            string formattedTime = stopwatch.Elapsed.ToString(@"hh\:mm\:ss");
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine($"execution time: {formattedTime}");
+            Console.ResetColor();
         }
 
         /// <summary>
@@ -87,7 +107,7 @@ namespace TestRunner
                         Console.ResetColor();
 
                         string command = $"dotnet test {threads[threadId].Dir} --filter {task} -v=normal --logger trx;LogFileName={task}.trx";
-                        TestRunner(command, threads[threadId].Output, threads[threadId].Error, threads[threadId].Lock);
+                        TestRunner(command, threads[threadId], threadId);
                     }
                     catch (Exception ex)
                     {
@@ -172,17 +192,17 @@ namespace TestRunner
         /// <summary>
         /// Executes the given test command and collects output and error streams
         /// </summary>
-        private static void TestRunner(string command, StringBuilder output, StringBuilder error, object threadLock)
+        private static void TestRunner(string command, ThreadState threadState, int threadId)
         {
             // This method runs the test command and handles the collection of output and error messages.
-            lock (threadLock)
+            lock (threadState.Lock)
             {
-                output.Clear();
+                threadState.Output.Clear();
             }
 
-            lock (threadLock)
+            lock (threadState.Lock)
             {
-                error.Clear();
+                threadState.Error.Clear();
             }
 
             using (Process proc = new Process())
@@ -199,8 +219,8 @@ namespace TestRunner
 
                 proc.StartInfo = psi;
 
-                proc.OutputDataReceived += (s, e) => { if (e.Data != null) lock (threadLock) { output.AppendLine("  " + e.Data); } };
-                proc.ErrorDataReceived += (s, e) => { if (e.Data != null) lock (threadLock) { error.AppendLine("  " + e.Data); } };
+                proc.OutputDataReceived += (s, e) => { if (e.Data != null) lock (threadState.Lock) { threadState.Output.AppendLine("  " + e.Data); } };
+                proc.ErrorDataReceived += (s, e) => { if (e.Data != null) lock (threadState.Lock) { threadState.Error.AppendLine("  " + e.Data); } };
 
                 proc.EnableRaisingEvents = true;
 
@@ -210,10 +230,14 @@ namespace TestRunner
                 proc.WaitForExit();
             }
 
-            lock (threadLock)
+            lock (threadState.Lock)
             {
-                ColoredPrinter(output);
-                ColoredPrinter(error);
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine($"Thread #{threadId + 1}: End of {threadState.Status}\n");
+                Console.ResetColor();
+
+                ColoredPrinter(threadState.Output);
+                ColoredPrinter(threadState.Error);
                 Console.ForegroundColor = ConsoleColor.DarkYellow;
                 Console.WriteLine("------------------------------------------------------------------------");
                 Console.ResetColor();
@@ -224,39 +248,39 @@ namespace TestRunner
         /// Prints the output and error results with color coding based on pass/fail status
         /// </summary>
         private static void ColoredPrinter(StringBuilder str)
-        {
-            // This method prints the results with color coding based on success (pass) or failure (fail)
-            Console.OutputEncoding = Encoding.UTF8;
-            foreach (var line in str.ToString().Split('\n'))
             {
-                foreach (var word in line.Split(' '))
+                // This method prints the results with color coding based on success (pass) or failure (fail)
+                Console.OutputEncoding = Encoding.UTF8;
+                foreach (var line in str.ToString().Split('\n'))
                 {
-                    if (Regex.IsMatch(word, @"\bpass(ed)?:?\b", RegexOptions.IgnoreCase))
+                    foreach (var word in line.Split(' '))
                     {
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        Console.Write($"✅ {word} ");
+                        if (Regex.IsMatch(word, @"\bpass(ed)?:?\b", RegexOptions.IgnoreCase))
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write($"✅ {word} ");
+                        }
+                        else if (Regex.IsMatch(word, @"\bfail(ed)?:?\b", RegexOptions.IgnoreCase))
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.Write($"❌ {word} ");
+                        }
+                        else if (Regex.IsMatch(word, @"\bresults:?\b", RegexOptions.IgnoreCase))
+                        {
+                            Console.ForegroundColor = ConsoleColor.DarkYellow;
+                            Console.Write($"📊 {word} ");
+                        }
+                        else
+                        {
+                            Console.ResetColor();
+                            Console.Write(word + " ");
+                        }
                     }
-                    else if (Regex.IsMatch(word, @"\bfail(ed)?:?\b", RegexOptions.IgnoreCase))
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.Write($"❌ {word} ");
-                    }
-                    else if (Regex.IsMatch(word, @"\bresults:?\b", RegexOptions.IgnoreCase))
-                    {
-                        Console.ForegroundColor = ConsoleColor.DarkYellow;
-                        Console.Write($"📊 {word} ");
-                    }
-                    else
-                    {
-                        Console.ResetColor();
-                        Console.Write(word + " ");
-                    }
+                    Console.WriteLine();
                 }
-                Console.WriteLine();
-            }
 
-            Console.ResetColor();
-        }
+                Console.ResetColor();
+            }
     }
 
     // Class to store the state of each thread
