71619919627d59cf2c9b391e8998cf4ea4eaf3b7
Iliya Forsati <iliyaforsati2005@gmail.com>
2025-05-18 10:50:42 +0330

    debug and update ui

--- runnerApp/setup.go
+++ runnerApp/setup.go
@@ -24,9 +24,24 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	defer conn.Close()
 
-	// اجرای تست‌رانر
-	cmd := exec.Command("cmd", "/C", ".\\TestRunner\\RTProSL-TestRunner.exe")
+	inputChan := make(chan [2][]byte)
 
+	go func() {
+		var inputs [2][]byte
+		for i := 0; i < 2; i++ {
+			_, msg, err := conn.ReadMessage()
+			if err != nil {
+				log.Println("Error reading from websocket:", err)
+				return
+			}
+			inputs[i] = append(msg, '\n')
+		}
+		inputChan <- inputs
+	}()
+
+	inputs := <-inputChan
+
+	cmd := exec.Command("cmd", "/C", ".\\TestRunner\\RTProSL-TestRunner.exe")
 	stdin, _ := cmd.StdinPipe()
 	stdout, _ := cmd.StdoutPipe()
 	cmd.Stderr = cmd.Stdout
@@ -34,18 +49,14 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 	err = cmd.Start()
 	if err != nil {
 		log.Println("Cannot start process:", err)
+		conn.WriteMessage(websocket.TextMessage, []byte("[Error: cannot start test runner]"))
 		return
 	}
 
-	// ✅ دادن ورودی‌ها به تست‌رانر
-	go func() {
-		// این مقادیر رو می‌تونی از کلاینت هم بفرستی
-		_, _ = stdin.Write([]byte("3\n"))
-		_, _ = stdin.Write([]byte("1\n"))
-		stdin.Close()
-	}()
+	stdin.Write(inputs[0])
+	stdin.Write(inputs[1])
+	stdin.Close()
 
-	// ✅ خواندن خروجی لحظه‌ای و فرستادن از طریق WebSocket
 	reader := bufio.NewReader(stdout)
 	for {
 		line, err := reader.ReadBytes('\n')
@@ -63,11 +74,12 @@ func openBrowser(url string) {
 	var cmd *exec.Cmd
 	switch runtime.GOOS {
 	case "windows":
-		cmd = exec.Command("rundll32", "url.dll,FileProtocolHandler", url)
-	case "darwin":
-		cmd = exec.Command("open", url)
-	default: // linux
-		cmd = exec.Command("xdg-open", url)
+		chromePath := "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
+		cmd = exec.Command(chromePath, "--incognito", url)
+	case "darwin": // macOS
+		cmd = exec.Command("open", "-a", "Google Chrome", "--args", "--incognito", url)
+	default: // Linux
+		cmd = exec.Command("chromium", "--incognito", url)
 	}
 	_ = cmd.Start()
 }
